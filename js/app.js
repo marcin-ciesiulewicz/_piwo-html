2c81b5acb3bfe36ecc72df0066f554bb23941b50
marcin-ciesiulewicz <marcin.cieiulewicz@wp.pl>
2018-10-05 17:08:52 +0200

    dodany scroll top

--- js/app.js
+++ js/app.js
@@ -3,6 +3,23 @@ $(document).ready(function () {
     $(window).scroll(function(){
 
         var wScroll = $(window).scrollTop();
+        var toTop = $('.toTop');
+
+        if( wScroll > 160 ){
+            toTop.fadeIn();
+        }else{
+            toTop.fadeOut();
+        }
+
+
+    });
+
+    $(".toTop").click(function (event) {
+        event.preventDefault();
+
+        $('html,body').animate({
+            scrollTop: $('.navigation').offset().top
+        }, 1000);
 
     });
 
